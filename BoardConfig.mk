4c1f45816bc303d9c63e88eb5d5ce85e95b622bd
HeroBuxx <me@herobuxx.xyz>
2021-06-22 14:34:20 +0700

    tulip: Rename back to tulip
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>
    Signed-off-by: HeroBuxx <me@buxxed.tk>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@
 # Inherit from sdm660-common
 include device/xiaomi/bouquet-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/twolip
+DEVICE_PATH := device/xiaomi/tulip
 
 # Duplicates
 BUILD_BROKEN_DUP_RULES := true
@@ -60,4 +60,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 VENDOR_SECURITY_PATCH := 2018-11-01
 
 # Inherit the proprietary files
-include vendor/xiaomi/twolip/BoardConfigVendor.mk
+include vendor/xiaomi/tulip/BoardConfigVendor.mk
